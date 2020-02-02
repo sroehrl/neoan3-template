281730caac5f61c12b911c01279c5d1f7506f1c1
neoan <stefan.roehrl@gmail.com>
2020-02-02 12:37:51 -0500

    Simplify maintenance

--- TemplateFunctions.php
+++ TemplateFunctions.php
@@ -6,6 +6,47 @@ namespace Neoan3\Apps;
 
 class TemplateFunctions
 {
+    private static function extractAttribute(\DOMElement $hit, $attribute)
+    {
+        // extract attribute
+        $parts = explode(' ', $hit->getAttribute($attribute));
+        // clean up
+        foreach ($parts as $i => $part) {
+            if (empty(trim($part))) {
+                unset($parts[$i]);
+            }
+        }
+        $parts = array_values($parts);
+        // remove attribute
+        $hit->removeAttribute($attribute);
+        // while string
+        return ['template' => Template::nodeStringify($hit), 'parts' => $parts];
+    }
+
+    static private function subContentGeneration(
+        \DOMDocument $domDocument,
+        \DOMElement $hit,
+        array $paramArray,
+        array $parts,
+        string $template
+    ) {
+        $newContent = '';
+        if (isset($paramArray[$parts[0]]) && !empty($paramArray[$parts[0]])) {
+            $subArray = [];
+            foreach ($paramArray[$parts[0]] as $key => $value) {
+                if (isset($parts[4])) {
+                    $subArray[$parts[2]] = $key;
+                    $subArray[$parts[4]] = $value;
+                } else {
+                    $subArray[$parts[2]] = $value;
+                }
+                $newContent .= Template::embrace($template, $subArray);
+            }
+            Template::clone($domDocument, $hit, $newContent);
+        }
+        return $newContent;
+    }
+
     /**
      * @param $content
      * @param $array
@@ -22,36 +63,33 @@ class TemplateFunctions
             return $content;
         }
         foreach ($hits as $hit) {
-            // extract attribute
-            $parts = explode(' ', $hit->getAttribute('n-for'));
-            // remove attribute
-            $hit->removeAttribute('n-for');
-            // while string
-            $template = Template::nodeStringify($hit);
-
-            // clean
-            foreach ($parts as $i => $part) {
-                if (empty(trim($part))) {
-                    unset($parts[$i]);
-                }
-            }
-            $parts = array_values($parts);
-            $newContent = '';
-            if (isset($array[$parts[0]]) && !empty($array[$parts[0]])) {
-                $subArray = [];
-                foreach ($array[$parts[0]] as $key => $value) {
-                    if (isset($parts[4])) {
-                        $subArray[$parts[2]] = $key;
-                        $subArray[$parts[4]] = $value;
-                    } else {
-                        $subArray[$parts[2]] = $value;
-                    }
-                    $newContent .= Template::embrace($template, $subArray);
-                }
-                Template::clone($doc, $hit, $newContent);
-            }
+            $extracted = self::extractAttribute($hit, 'n-for');
+            self::subContentGeneration($doc, $hit, $array, $extracted['parts'], $extracted['template']);
         }
         return $doc->saveHTML();
+    }
+    private static function evaluateTypedCondition(array $flatArray, $expression){
+        $bool = true;
+        foreach ($flatArray as $key => $value) {
+            if (strpos($expression, $key) !== false) {
+                switch (gettype($flatArray[$key])) {
+                    case 'boolean':
+                        $expression = str_replace($key, $flatArray[$key] ? 'true' : 'false', $expression);
+                        break;
+                    case 'NULL':
+                        $expression = str_replace($key, 'false', $expression);
+                        break;
+                    case 'string':
+                        $expression = str_replace($key, '"' . $flatArray[$key] . '"', $expression);
+                        break;
+                    default:
+                        $expression = str_replace($key, $flatArray[$key], $expression);
+                        break;
+                }
+                $bool = eval("return $expression;");
+            }
+        }
+        return $bool;
     }
 
     /**
@@ -72,27 +110,8 @@ class TemplateFunctions
 
         foreach ($hits as $hit) {
             $expression = $hit->getAttribute('n-if');
-            $bool = true;
             $array = Template::flattenArray($array);
-            foreach ($array as $key => $value) {
-                if (strpos($expression, $key) !== false) {
-                    switch (gettype($array[$key])) {
-                        case 'boolean':
-                            $expression = str_replace($key, $array[$key] ? 'true' : 'false', $expression);
-                            break;
-                        case 'NULL':
-                            $expression = str_replace($key, 'false', $expression);
-                            break;
-                        case 'string':
-                            $expression = str_replace($key, '"' . $array[$key] . '"', $expression);
-                            break;
-                        default:
-                            $expression = str_replace($key, $array[$key], $expression);
-                            break;
-                    }
-                    $bool = eval("return $expression;");
-                }
-            }
+            $bool = self::evaluateTypedCondition($array, $expression);
 
             if (!$bool) {
                 $hit->parentNode->removeChild($hit);
